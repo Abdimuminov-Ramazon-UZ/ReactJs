476cd062a9581317b5ade71b9c50f1c2f9b82e34
HpEliteBook <generallg5390@gmail.com>
2025-12-03 18:27:21 +0500

    CRUD

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,22 +1,8 @@
-//homeworks with fruit
-// import React from 'react'
-// class Fruit extends React.Component{
 
-// 	render(){
-// 		console.log(this.props)
-// 		return (
-// 			<div class="container">
-// 						{this.props.data?.id} : {this.props.data?.name}
-// 			</div>
-// 		)
-// 	}
-// }
-// export default Fruit
 import React from 'react'
 import { student } from "./mock.js"
 class State extends React.Component{
-	//we create constructor function for Lifecycle which is we connect with React.components elements
-	//we have react rule so constructor run first though it writes under render
+
 	constructor(props){
 		super(props)
 		this.state={
@@ -26,93 +12,81 @@ class State extends React.Component{
 			checked:true,
 			name:"Max",
 			surname:"Brad",
+			status:"",
 			data:student
 		}
 	}
 	render(){
 
-		// const plus=()=>{
-		// 	// this.state.count=this.state.count+1
-		// 	//we change state variables with setState function and it runs render function if we click button
-		// 	if (this.state.count<10) {
-		// 		this.setState({count:this.state.count+1})
-		// 	}
-		// 	console.log(this.state.count)
-		// }
-		// const minus=()=>{
-		// 	//we add conditions for control limit
-		// 	if (this.state.count>0) {
-		// 	this.setState({count:this.state.count-1})
-		// 	}
-		// }
-		// we get default DOM parametr which is event
-		// const onchange=(event)=>{
-		// 	this.setState({title:event.target.value})
-		// 	console.log(event.target.value)
-		// }
-			// const onchange=(event)=>{
-			// 	// event.target.value={}
-			// 	console.log(event,this.state)
-				//we get name title for input element and we get universal onchange funtion important view is the same keys we get similar names for state properties and input's name title
-		// 		console.log(event.target.name)
-		// 	this.setState({[event.target.name]:event.target.value})
-		// 	console.log(event.target.value)
-		// }
-		// const onselect=(event)=>{
-		// 	this.setState({option:event.target.value})
-		// }
-		// const oncheck=(event)=>{
-		// 	if(event.target.checked){
-		// 		this.setState({count:1})
-		// 	}
-		// }
-		// const onchangename=e=>{
-		// 	console.log(e)//e is special object for react DOM 
-		// 	this.setState({name:e.target.value})
-		// }
-		// 	const onchangesurname=e=>{
-		// 	// this.setState({name:e.target.value})
-		// 	//we call e 's target
-		// 	this.setState({surname:e?.target?.value})
-
-		// }
-		// we filter data elements with name
 		const onfilter=e=>{
 			console.log(e)
 			let res=student.filter(value=>value.name.includes(e?.target?.value))
 			this.setState({data:res})
 		}
+		const ondelete=id=>{
+			console.log(id)
+			let res =this.state.data.filter((value)=>value.id!==id)
+			this.setState({data:res})
+		}
+		const onchange=e=>{
+			console.log(e.target)
+			this.setState({[e.target.name]:e.target.value})
+		}
+		//Add datas first off Create element
+		const onadd=e=>{
+			console.log(e)
+			let user={
+				id:Date.now(),
+				name:this.state.name,
+				status:this.state.status
+			}
+			this.setState({name:"",status:""})
+			console.log(user)
+			this.setState({data:[...this.state.data,user],name:"",status:""})
+		}
 		return (
 			<div>
-			{/* // 	<h1>State {this.state.count}</h1>
-			//    <h1>name: {this.state.name}</h1>
-			//    <h1>surname: {this.state.surname}</h1>
-			// 	<input name='name' onChange={onchange} type="text" placeholder='name' /> */}
-				{/* first input is returns free because we give default parametr to event */}
-						{/* <input onChange={()=>{onchangesurname("hey")}} type="text" placeholder='surname' /> */}
-						{/* second input obeys React's rules and it changes surname
-						<input onChange={onchangesurname} type="text" placeholder='surname' /> */}
-						{/* third input changes surname too because we call function as callback function though we give it e as event 
-						<input onChange={(e)=>{onchangesurname(e,"hey")}} type="text" placeholder='surname' /> */}
-				{/* <h2>{this.state.title} {this.state.option}</h2>
-				<input onChange={onchange} placeholder='name' type="text" />
-				<button onClick={plus}>+</button>
-				<button onClick={minus}>-</button>
-				<select onChange={onselect} name="" id="">
-					<option value="male">male</option>
-					<option value="female">female</option>
-
-				</select>
-				<input onChange={oncheck} type="checkbox" /> */}
-				<input onChange={onfilter} type="text"  placeholder='Search'/>
+	       <input value={this.state.name} onChange={onchange} name="name" type="text"  placeholder='name'/>
+	       <input value={this.state.status} onChange={onchange} name="status" type="text"  placeholder='status'/>
+					<button onClick={onadd}>add</button>
+				<input onChange={onfilter} type="text"  
+				placeholder='Search'/>
 				<hr />
-				{
+				<table border="1px" width={"100%"}>
+					<thead>
+						<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Status</th>
+						<th>Delete</th>
+						<th>Edit</th>
+					</tr>
+					</thead>
+					<tbody>
+							{
+								this.state.data.length?(
 					this.state.data.map(({id,name,status})=>{
 						return  (
-							<h1>{id}:{name} {status}</h1>
+						<tr>
+							<td>{id}</td>
+							<td>{name}</td>
+							<td>{status}</td>
+							{/* if we don't get here id we don't get important id id come heren map's value */}
+							<td><button onClick={()=>ondelete(id)}>Delete</button></td>
+							<td><button>Edit</button></td>
+
+						</tr>
+							
 						)
 					})
-				}
+	 ):(
+		<tr><th colSpan={5}><h1>No Data</h1></th></tr>
+	  )
+	}
+	
+					</tbody>
+				</table>
+			
 			</div>
 		)
 	}
